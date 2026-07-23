096258d950858821834c2d6639c456895bf37fbb
xianjh <925066365@qq.com>
2026-07-23 14:50:42 +0800

    v4.0 重构TextChunker

--- knowledge/chunkers/text_chunker.py
+++ knowledge/chunkers/text_chunker.py
@@ -2,10 +2,60 @@ from knowledge.document import Document
 
 
 class TextChunker:
-    def __init__(self,chunker_size=500,overlap=100):
-        self.chunker_size = chunker_size
+
+
+    def __init__(
+        self,
+        chunk_size=500,
+        overlap=100
+    ):
+
+        self.chunk_size = chunk_size
         self.overlap = overlap
 
-    def split(self, documents:list[Document])->list[Document]:
 
-        pass 
+    def split(
+        self,
+        documents: list[Document]
+    ) -> list[Document]:
+
+        chunks = []
+
+
+        for doc in documents:
+
+            text = doc.page_content
+
+            start = 0
+
+            index = 1
+
+
+            while start < len(text):
+
+                end = start + self.chunk_size
+
+                chunk_text = text[start:end]
+
+
+                metadata = {
+                    **doc.metadata,
+                    "chunk": index
+                }
+
+
+                chunk = Document(
+                    page_content=chunk_text,
+                    metadata=metadata
+                )
+
+
+                chunks.append(chunk)
+
+
+                index += 1
+
+                start += self.chunk_size - self.overlap
+
+
+        return chunks
